44a1849c789eca7387f1bb5056543359eebf0de5
Markel Adánez <m.adanez@opendeusto.es>
2025-10-27 11:33:37 +0100

    arreglado @Service

--- proyecto/src/main/java/es/deusto/sd/proyecto/Service/loginService.java
+++ proyecto/src/main/java/es/deusto/sd/proyecto/Service/loginService.java
@@ -1,7 +1,10 @@
 package es.deusto.sd.proyecto.Service;
 
+import org.springframework.stereotype.Service;
+
 import es.deusto.sd.proyecto.DTO.userDTO;
 
+@Service
 public class loginService {
 
     public loginService(){
