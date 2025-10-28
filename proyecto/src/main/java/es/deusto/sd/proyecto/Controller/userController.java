045b14805c4afd989e5e954ebbddd15bf64e69cd
Markel Adánez <m.adanez@opendeusto.es>
2025-10-28 08:27:40 +0100

    juntados login y register en user

--- proyecto/src/main/java/es/deusto/sd/proyecto/Controller/userController.java
+++ proyecto/src/main/java/es/deusto/sd/proyecto/Controller/userController.java
@@ -1,33 +1,34 @@
 package es.deusto.sd.proyecto.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.deusto.sd.proyecto.DTO.userDTO;
-import es.deusto.sd.proyecto.Service.loginService;
+import es.deusto.sd.proyecto.Service.userService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
-@RequestMapping("/login")
-@Tag(name="Login", description = "Login to the App")
-public class loginController {
-    private final loginService loginService;
+@RequestMapping("/user")
+@Tag(name="User", description = "Different Actions for user")
+public class userController {
+    private final userService userService;
 
-    public loginController(loginService loginService){
-        this.loginService=loginService;
+    public userController(userService userService){
+        this.userService=userService;
     }
 
 
-
+    //LOGIN
+    @RequestMapping("/login")
     @Operation(
         summary = "Login to the App",
         description = "Returns a Token to use the app"
@@ -49,8 +50,31 @@ public class loginController {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);//401
             }
 
-        String token=loginService.generateToken(userDTO);
+        String token=userService.generateToken(userDTO);
         return new ResponseEntity<>(token, HttpStatus.OK);//200
     }
-    
+
+    //REGISTER
+
+    @RequestMapping("/register")
+    @Operation(
+        summary = "Register in the App",
+        description = "Creates a User for future Logins"
+    )
+    @ApiResponse(responseCode = "200", description = "OK")
+
+    @PostMapping
+    public ResponseEntity<String> register(
+        @Parameter(description="Username and password",required = true)
+        @RequestBody userDTO userDTO) {
+
+        userService.registerUser(userDTO);
+            
+        return new ResponseEntity<>(HttpStatus.OK);//200
+        }
+
+
+    //LOGOUT
+
+
 }
--- proyecto/src/main/java/es/deusto/sd/proyecto/Service/userService.java
+++ proyecto/src/main/java/es/deusto/sd/proyecto/Service/userService.java
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Service;
 import es.deusto.sd.proyecto.DTO.userDTO;
 
 @Service
-public class loginService {
+public class userService {
 
-    public loginService(){
-
+    public userService() {
     }
 
     public String generateToken(userDTO userDto){
         return "";
     }
-    
+
+    public void registerUser(userDTO userDTO){
+
+    }
 }
